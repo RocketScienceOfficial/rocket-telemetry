3ea695e9ddbdb2b97eb4d1a2d5b6ebb746eee842
Filip Gawlik <filippo.gawlik@gmail.com>
2024-08-22 13:36:47 +0200

    Fix orientation

--- Assets/Code/Controllers/Other/OrientationController.cs
+++ Assets/Code/Controllers/Other/OrientationController.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class OrientationController : MonoBehaviour, IDataRecipient
 {
-    private Quaternion _baseQuat;
-
-    private void Start()
-    {
-        _baseQuat = Quaternion.Euler(90f, 0f, -90f);
-    }
-
     public void OnSetData(RecipientData data)
     {
-        transform.rotation = _baseQuat * new Quaternion(data.qx, data.qy, data.qz, data.qw);
+        transform.localRotation = new Quaternion(data.qx, data.qy, data.qz, data.qw);
     }
 }
